ab9d91ed41de3e4143484118a2959d1324a4f4ce
Andre Vlot <andrevlot@gmail.com>
2022-04-28 16:47:47 +0200

    fedex and gls images added

--- montapacking-checkout.php
+++ montapacking-checkout.php
@@ -3,7 +3,7 @@
  * Plugin Name: Monta Checkout
  * Plugin URI: https://github.com/Montapacking/woocommerce-monta-checkout
  * Description: Monta Check-out extension
- * Version: 1.37
+ * Version: 1.38
  * Author: Monta
  * Author URI: https://www.monta.nl/
  * Developer: Monta
