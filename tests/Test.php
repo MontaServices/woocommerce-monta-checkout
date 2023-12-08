daf547c5bd6a4b085a203fb9951d2850961b3b56
Wilco Broer <Wilco.Broer@monta.nl>
2023-12-08 14:22:40 +0100

    remove comments

--- tests/Test.php
+++ tests/Test.php
@@ -10,19 +10,7 @@ final class StackTest extends TestCase
 {
     public function testPushAndPop(): void
     {
-//        $montacking = new Montapacking;
-
-        $stack = [];
-        $this->assertSame(0, count($stack));
-
-        array_push($stack, 'foo');
-        $this->assertSame('foo', $stack[count($stack)-1]);
-        $this->assertSame(1, count($stack));
-
-        $this->assertSame('foo', array_pop($stack));
-        $this->assertSame(0, count($stack));
-
-        $settings = new Settings('Demoshop.nl', 'wilco@montapacking.nl', '4%9_BYN%MCGP', true, 10, 'AIzaSyDc1ruiAmgYhuU6d925mrPCaBsyMAHT0LI', 2);
+        $settings = new Settings('Demoshop.nl', 'wilco@montapacking.nl', '', true, 10, 'AIzaSyDc1ruiAmgYhuU6d925mrPCaBsyMAHT0LI', 2);
 
         $api = new MontapackingShipping($settings, 'nl-NL');
 
@@ -39,9 +27,6 @@ final class StackTest extends TestCase
 
         $results = $api->getShippingOptions();
 
-//        $test1 = $montacking->get_frames();
-
-//        $this->assertNotSame(null, $results);
         $this->assertNotNull($results);
         $this->assertTrue( sizeof($results['DeliveryOptions']) > 0);
         $this->assertTrue( sizeof($results['PickupOptions']) > 0);
