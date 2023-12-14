ae0cbfa87318972cb7bfe40ef03e3e6e13a2fd06
Kevin Kroos <kevin.kroos@monta.nl>
2023-12-14 11:56:32 +0100

    Added integration tests and .env.example for environment variables

--- montapacking-config.php
+++ montapacking-config.php
@@ -1,7 +1,10 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) {
-    exit; // Exit if accessed directly
-}
+//if ( ! defined( 'ABSPATH' ) ) {
+//    exit; // Exit if accessed directly
+//}
 
 
 require_once('vendor/autoload.php');
+
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__FILE__));
+$dotenv->load();
