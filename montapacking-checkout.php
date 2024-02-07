1a4055ecb93f3319813c7a48920db9bb886e8635
Kevin Kroos <kevin.kroos@monta.nl>
2024-02-07 09:44:29 +0100

    Updated version

--- montapacking-checkout.php
+++ montapacking-checkout.php
@@ -3,7 +3,7 @@
  * Plugin Name: Monta Checkout
  * Plugin URI: https://github.com/Montapacking/woocommerce-monta-checkout
  * Description: Monta Check-out extension
- * Version: 1.58.18
+ * Version: 1.58.19
  * Author: Monta
  * Author URI: https://www.monta.nl/
  * Developer: Monta
