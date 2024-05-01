8fcd3218950d51412cbad036089b264151073a90
Wilco Broer <wilco.broer@monta.nl>
2024-05-01 16:25:22 +0200

    Update version

--- montapacking-checkout.php
+++ montapacking-checkout.php
@@ -3,7 +3,7 @@
  * Plugin Name: Monta Checkout
  * Plugin URI: https://github.com/Montapacking/woocommerce-monta-checkout
  * Description: Monta Check-out extension
- * Version: 1.58.32
+ * Version: 1.58.35
  * Author: Monta
  * Author URI: https://www.monta.nl/
  * Developer: Monta
