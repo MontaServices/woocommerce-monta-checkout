eb750a71aed711ab3f8a14c1dd3b48e6c595bbfb
Jacco Amersfoort <jacco.amersfoort@monta.nl>
2025-07-23 09:29:28 +0200

    Revert "#60086 Flip if-statement around checking frame options"
    This reverts commit b1a46fe3a16dd2e6f8b44e5736fe526540a4d082.

--- src/Packing.php
+++ src/Packing.php
@@ -520,15 +520,14 @@ class Packing
 
                         ## Check of timeframe opties heeft
                         foreach ($frames as $frame) {
-                            if (is_array($options = $frame->options)) {
-                                foreach ($options as $option) {
-                                    if ($option->code == $shipper) {
-                                        $method = $option;
-                                        break;
-                                    }
-                                }
-                            } else {
+                            if (!is_array($options = $frame->options)) {
                                 throw new \Exception("Shipper options should be an array, actual: " . var_export($options, true));
+                            }
+                            foreach ($options as $option) {
+                                if ($option->code == $shipper) {
+                                    $method = $option;
+                                    break;
+                                }
                             }
                         }
 
